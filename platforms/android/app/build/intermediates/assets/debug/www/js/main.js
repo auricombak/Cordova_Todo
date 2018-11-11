045028de9958c42a8357b8b7da7685e2ce587a1e
auricombak <lois.fwee@hotmail.fr>
2018-11-11 01:32:32 +0100

    Add time/category/adress + redesign list

--- platforms/android/app/build/intermediates/assets/debug/www/js/main.js
+++ platforms/android/app/build/intermediates/assets/debug/www/js/main.js
@@ -7,10 +7,23 @@ var i = 0;
 //Check for todos
 if(localStorage.getItem('todos') != null){
 	//Loop through and output li items
+	var html_list_view_map = {};
 	$.each(todoList,function(key, value){
-		$('#todos').prepend('<li id="task-'+i+'"><a id="todo_link" href="#edit" data-todo_name ="'+value.todo_name+'" data-todo_date="'+value.todo_date +'" data-todo_category="'+value.todo_category+'" data-todo_adress="'+value.todo_adress+'">'+'<span>'+ value.todo_name+'</span><span>'+value.todo_date+'</span><span>'+value.todo_category+'</span><span>'+value.todo_adress+'</span></a></li>');
+		if(!html_list_view_map.hasOwnProperty(value.todo_date)){
+			html_list_view_map[value.todo_date] = '<li data-role="list-divider id="'+value.todo_date+'">'+value.todo_date+'<span class="ui-li-count">2</span></li><li id="task-'+i+'"><a id="todo_link" href="#edit" data-todo_name ="'+value.todo_name+'" data-todo_date="'+value.todo_date +'" data-todo_category="'+value.todo_category+'" data-todo_adress="'+value.todo_adress+'" data-todo_adress="'+value.todo_category+'" data-todo_time="'+value.todo_time+'">'+'<h2>'+value.todo_name+'</h2><p><strong>'+value.todo_adress+'</strong></p><p>'+value.todo_category+'</p><p class="ui-li-aside"><strong>'+value.todo_time+'</strong></p></a></li>';
+		}else{
+			html_list_view_map[value.todo_date] += '<li id="task-'+i+'"><a id="todo_link" href="#edit" data-todo_name ="'+value.todo_name+'" data-todo_date="'+value.todo_date +'" data-todo_category="'+value.todo_category+'" data-todo_adress="'+value.todo_adress+'" data-todo_adress="'+value.todo_category+'" data-todo_time="'+value.todo_time+'">'+'<h2>'+value.todo_name+'</h2><p><strong>'+value.todo_adress+'</strong></p><p>'+value.todo_category+'</p><p class="ui-li-aside"><strong>'+value.todo_time+'</strong></p></a></li>';
+		}
 		i++;
 	});
+
+	//For each date event
+	for (var key_date in html_list_view_map) {
+ 			if (html_list_view_map.hasOwnProperty(key_date)) {
+					$('#todos').append(html_list_view_map[key_date]);
+ 			}
+	}
+
 	//Refresh
 	$('#todos').listview('refresh');
 }
@@ -20,6 +33,7 @@ $('#add_form').submit(function(){
 	//Get submitted values
 	var todo_name = $('#todo_name').val();
 	var todo_date = $('#todo_date').val();
+	var todo_time = $('#todo_time').val();
 	var todo_category = $('#todo_category').val();
 	var todo_adress = $('#todo_adress').val();
 
@@ -39,6 +53,7 @@ $('#add_form').submit(function(){
 		var new_todo = {
 			"todo_name": todo_name,
 			"todo_date": todo_date,
+			"todo_time": todo_time,
 			"todo_category": todo_category,
 			"todo_adress" : todo_adress
 		}
@@ -52,6 +67,7 @@ $('#add_form').submit(function(){
 	$('#edit_form').submit(function(){
 		currentTodoName = localStorage.getItem('currentTodoName');
 		currentTodoDate = localStorage.getItem('currentTodoDate');
+		currentTodoTime = localStorage.getItem('currentTodoTime');
 		currentTodoCategory = localStorage.getItem('currentTodoCategory');
 		currentTodoAdress = localStorage.getItem('currentTodoAdress');
 
@@ -68,6 +84,7 @@ $('#add_form').submit(function(){
 		//Create a new todo
 		var todo_name_edit = $('#todo_name_edit').val();
 		var todo_date_edit = $('#todo_date_edit').val();
+		var todo_date_time = $('#todo_date_time').val();
 		var todo_category_edit = $('#todo_category_edit').val();
 		var todo_adress_edit = $('#todo_adress_edit').val();
 
@@ -77,6 +94,7 @@ $('#add_form').submit(function(){
 		var update_todo = {
 			"todo_name": todo_name_edit,
 			"todo_date": todo_date_edit,
+			"todo_time": todo_date_time,
 			"todo_category": todo_category_edit,
 			"todo_adress" : todo_adress_edit
 		};
@@ -89,6 +107,7 @@ $('#add_form').submit(function(){
 	$('#edit_form').on('click','#delete',function(){
 		currentTodoName = localStorage.getItem('currentTodoName');
 		currentTodoDate = localStorage.getItem('currentTodoDate');
+		currentTodoTime = localStorage.getItem('currentTodoTime');
 		currentTodoCategory = localStorage.getItem('currentTodoCategory');
 		currentTodoAdress = localStorage.getItem('currentTodoAdress');
 
@@ -106,6 +125,7 @@ $('#add_form').submit(function(){
 	$('#todos').on('click','#todo_link',function(){
 		localStorage.setItem('currentTodoName',$(this).data('todo_name'));
 		localStorage.setItem('currentTodoDate',$(this).data('todo_date'));
+		localStorage.setItem('currentTodoTime',$(this).data('todo_time'));
 		localStorage.setItem('currentTodoCategory',$(this).data('todo_category'));
 		localStorage.setItem('currentTodoAdress',$(this).data('todo_adress'));
 	});
@@ -114,11 +134,13 @@ $('#add_form').submit(function(){
 	$(document).on('pageshow','#edit',function(){
 		currentTodoName = localStorage.getItem('currentTodoName');
 		currentTodoDate = localStorage.getItem('currentTodoDate');
+		currentTodoTime = localStorage.getItem('currentTodoTime');
 		currentTodoCategory = localStorage.getItem('currentTodoCategory');
 		currentTodoAdress = localStorage.getItem('currentTodoAdress');
 
 		$('#edit_form input[name=todo_name_edit]',this).val(currentTodoName);
 		$('#edit_form input[name=todo_date_edit]',this).val(currentTodoDate);
+		$('#edit_form input[name=todo_time_edit]',this).val(currentTodoTime);
 		$('#edit_form input[name=todo_category_edit]',this).val(currentTodoCategory);
 		$('#edit_form input[name=todo_adress_edit]',this).val(currentTodoAdress);
 	});
